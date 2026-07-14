3ebc48f1fa4e3a3ceebe2959016244c80a40bafb
appleflyerv3 <164886846+appleflyerv3@users.noreply.github.com>
2026-07-15 00:45:17 +0800

    refactor, chore: bump citadel submodule hash, change env to not have prints enabled by default
    Signed-off-by: appleflyerv3 <164886846+appleflyerv3@users.noreply.github.com>

--- env.py
+++ env.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from lib.logger import GscemuLoggerSettings
 
 GSCEMULATOR_LOGGER_SETTINGS = GscemuLoggerSettings(
-    global_switch=True,
+    global_switch=False,
     debug_prints=True,
     info_prints=True,
     warning_prints=True,
